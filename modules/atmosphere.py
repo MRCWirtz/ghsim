Fix bug in units of propagation (m <-> km) / Make co2 data available in repository

modules/atmosphere.py
@@ -1,21 +1,21 @@
 import numpy as np
 
-h0 = 1e4    # scale height for atmosphere, in m
+h0 = 10    # scale height for atmosphere, in km
 mass_density = 1.2041  # kg/m^3
 number_density = 2.504e25    # molecules per m^3
 
 
-def get_propagation_distance(attenuation, theta, hs):
+def get_propagation_distance(attenuation, theta, hs, random=True):
     # mask for rays that are propagated
     mask = (attenuation > 0)
-    alpha = attenuation * h0 / np.cos(theta)
-    u = np.random.random(theta.size)
-    inner_log = np.zeros(theta.size)
+    alpha = np.cos(theta[mask]) / attenuation[mask] / h0 / np.exp(-hs[mask] / h0)
+    u = np.random.random(np.sum(mask)) if random else np.ones(np.sum(mask)) * 0.5
+    inner_log = -np.ones(theta.size)
     # check for rays that have interaction in atmosphere
-    inner_log[mask] = 1 + np.log(1-u[mask]) / alpha[mask]
+    inner_log[mask] = 1 + alpha * np.log(1-u)
     # if the argument of the logarithm becomes smaller zero, the ray left the atmosphere
     mask = mask & (inner_log > 0)
-    distance = -1/np.cos(theta[mask]) * (h0 * np.log(inner_log[inner_log > 0]) + hs[mask])
+    distance = -h0/np.cos(theta[mask]) * np.log(inner_log[mask])
     return mask, distance
 
 
@@ -29,24 +29,30 @@ class Atmosphere:
     def get_distance(self, nu, theta, hs):
         attenuation = np.zeros(nu.shape)
         for gas in self.gases:
-            attenuation += number_density * gas.get_attenuation(nu)
+            attenuation += number_density * (1e-4 * gas.get_attenuation(nu)) * 1e3  # in (1 / km)
         return get_propagation_distance(attenuation, theta, hs)
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    # show as a function of the cross section which fraction of upgoing
-    # rays are blocked in the atsmosphere
-    cross_sections = np.logspace(-30, -18, 100000)  # in cm^2
-    attenuation = cross_sections * 1e-4 * number_density * 0.0004
-    block = 1 - np.exp(-attenuation*h0)
-    plt.plot(cross_sections, block, color='k')
-    for frac in [0.1, 0.01, 0.001]:
-        plt.axhline(frac, color='red', linestyle='dotted')
-        idx = np.argmin(np.abs(block - frac))
-        plt.axvline(cross_sections[idx], color='red', linestyle='dotted')
+    n = 10000
+    attenuation = np.logspace(-6, 3, n)
+    for h in [0, 1, 3, 10]:
+        mask, distance = get_propagation_distance(attenuation, np.zeros(n), h * np.ones(n), random=False)
+        plt.plot(attenuation[mask], distance, label='hs: %i km' % h)
+    plt.legend(fontsize=16)
+    plt.xlabel('attenuation [1/km]', fontsize=14)
+    plt.ylabel('distance [km]', fontsize=14)
     plt.xscale('log')
     plt.yscale('log')
-    plt.xlabel('cross section [cm^2]', fontsize=14)
-    plt.ylabel('fraction absorped', fontsize=14)
+    plt.show()
+
+    for h in [0, 1, 3, 10]:
+        mask, distance = get_propagation_distance(attenuation, 1e-5 + np.pi/2 * np.ones(n), h * np.ones(n), random=False)
+        plt.plot(attenuation[mask], distance, label='hs: %i km' % h)
+    plt.legend(fontsize=16)
+    plt.xlabel('attenuation [1/km]', fontsize=14)
+    plt.ylabel('distance [km]', fontsize=14)
+    plt.xscale('log')
+    plt.yscale('log')
     plt.show()

modules/gases.py
@@ -15,7 +15,7 @@ class Gas:
         self.data = np.load(path, allow_pickle=True)
         self.interp = self.data['interp_indices'][()]
         self.wn_left, self.wn_right = self.data['wave_numbers_left'], self.data['wave_numbers_right']
-        self.cross_section = self.data['cross_sections'] * 1e-4  # in m^2
+        self.cross_section = self.data['cross_sections']  # in cm^2
 
     def get_attenuation(self, nu):
         wn = nu2wn(nu)
@@ -25,7 +25,7 @@ class Gas:
             indices[indices < 0] = 0
             mask = (wn >= self.wn_left[indices]) & (wn < self.wn_right[indices])
             cross_section[mask] = self.cross_section[indices[mask]]
-        return cross_section * self.fraction
+        return cross_section * self.fraction    # in cm^2
 
 
 class CarbonDioxide(Gas):
@@ -39,10 +39,28 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     wn_all = np.linspace(1, 2500, 1000000)
     nu_all = c * 1e2 * wn_all
-    gas = CarbonDioxide(fraction=0.0004)
+    gas = CarbonDioxide(fraction=1)
     cross_sections = gas.get_attenuation(nu_all)
-    plt.plot(wn_all, cross_sections * 1e4, color='k')
+    plt.plot(wn_all, cross_sections, color='k')
     plt.yscale('log')
     plt.xlabel('wave number [1 / cm]', fontsize=16)
     plt.ylabel('cross section [cm^2]', fontsize=16)
+    plt.show()
+
+    # show as a function of the cross section which fraction of upgoing
+    # rays are blocked in the atsmosphere
+    cross_sections = np.logspace(-30, -18, 100000)  # in cm^2
+    number_density = 2.504e25    # molecules per m^3
+    gas = CarbonDioxide(fraction=0.0004)
+    attenuation = cross_sections * 1e-4 * gas.fraction * number_density
+    block = 1 - np.exp(-attenuation * 1e4)
+    plt.plot(cross_sections, block, color='k')
+    for frac in [0.1, 0.01, 0.001]:
+        plt.axhline(frac, color='red', linestyle='dotted')
+        idx = np.argmin(np.abs(block - frac))
+        plt.axvline(cross_sections[idx], color='red', linestyle='dotted')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('cross section [cm^2]', fontsize=14)
+    plt.ylabel('fraction absorped', fontsize=14)
     plt.show()

modules/radiation.py
@@ -4,11 +4,11 @@ from modules import coord, physics
 
 class Rays:
 
-    def __init__(self, atm, re, n, rmax):
+    def __init__(self, atm, re, n, hmax):
 
         self.nmax = n
         self.n_active = 0
-        self.rmax = rmax
+        self.rmax = re + hmax
         self.re = re
         self.atm = atm
         self.x = re * coord.rand_vec(n)     # position vector on Earth surface
@@ -44,7 +44,7 @@ class Rays:
                 self._interact_atmosphere()
                 e_loss += self._leave_atmosphere()
                 self._absorb_surface()
-        print('Absorbed: %i (%.5f)' % (self.absorbed, self.absorbed / max(1, ntot)))
+        print('Batches absorbed: %i (fraction of emitted: %.5f)' % (self.absorbed, self.absorbed / max(1, ntot)))
         return e_loss
 
     def _propagate(self):
@@ -63,7 +63,7 @@ class Rays:
         self.n_active -= np.sum(mask_leave)
         self.active[mask_leave] = False
         self.collect_leave[mask_leave] = False
-        return physics.h * np.sum(self.nu[mask_leave])
+        return physics.h * np.sum(self.nu[mask_leave])  # in Joule
 
     def _absorb_surface(self):
         # check for absorbed rays

modules/surface.py
@@ -2,32 +2,34 @@ import numpy as np
 
 
 me = 5.97e24    # mass earth, in kg
-me = 1e10    # mass earth, in kg
-re = 6.371e6    # radius earth, in m
+re = 6371    # radius earth, in km
 c_heat = 1  # specific heat capacity, in J/(kg·K)
 solar_constant = 1367   # W / m^2
-photon_multiplier = 1e16    # batches of photons emitted at the same time
+photon_multiplier = 1e40    # batches of photons emitted at the same time
 photon_constant = 1.072e15 / photon_multiplier
+
+time_acceleration = 10. * (365 * 24 * 3600)    # 1 corresponds to a second
 
 
 class Surface:
 
-    def __init__(self, t=0, albedo=0.306, re=6371):
+    def __init__(self, t=0, albedo=0.306):
         self.t = t
         self.t_hist = []
         self.delta_t = None
         self.albedo = albedo
         self.re = re
-        self.cross_area = np.pi * self.re**2
-        self.surface_area = 4 * np.pi * self.re**2
+        self.cross_area = np.pi * (1e3 * self.re)**2
+        self.surface_area = 4 * np.pi * (1e3 * self.re)**2
 
     def _external_heat(self):
-        self.delta_t = (1 - self.albedo) * solar_constant * self.cross_area / c_heat / me
+        self.delta_t = (1 - self.albedo) * solar_constant * time_acceleration * self.cross_area / c_heat / me
 
     def _radiate(self, rad):
-        n_emit = np.random.poisson(photon_constant * self.t**3)
-        energy_emission = photon_multiplier * rad.radiate(self.t, n_emit)
-        self.delta_t -= energy_emission * self.surface_area / c_heat / me
+        n_emit_mean = time_acceleration * self.surface_area * photon_constant * self.t**3
+        n_emit = np.random.poisson(n_emit_mean)
+        energy_emission = photon_multiplier * rad.radiate(self.t, n_emit)   # total energy loss in J
+        self.delta_t -= energy_emission / c_heat / me
 
     def _change_temperature(self):
         if self.delta_t is None: